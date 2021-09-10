1aa4cb745cedf7d3cdc79c788c04309e556069dc
Filip Broniek <broniekf@gmail.com>
2021-09-10 15:08:19 +0200

    Dodaj pliki projektów.

---
+++ MinimalAPI/DishesProvider.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MinimalAPI;
+public class DishesProvider
+{
+    public IEnumerable<Dish> GetDishes()
+    {
+        yield return new Dish { Name = "Dish1", Price = 12 };
+
+        yield return new Dish { Name = "Dish2", Price = 14.12M };
+    }
+}
---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using MinimalAPI;
+
+using (var db = new DishContext())
+{
+
+}
+
+var builder = WebApplication.CreateBuilder();
+
+var app = builder.Build();
+
+app.MapGet("/getDishes", () => new DishesProvider().GetDishes());
+
+app.Run();
