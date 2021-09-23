86c7eb47a2064dc71fc851534f27b90cdc43cf46
Filip Broniek <broniekf@gmail.com>
2021-09-23 12:48:37 +0200

    undo

--- MinimalAPI/Services/DishService.cs
+++ MinimalAPI/Services/DishService.cs
@@ -1,5 +1,7 @@
-﻿namespace MinimalAPI;
+﻿
+using Microsoft.EntityFrameworkCore;
 
+namespace MinimalAPI;
 public class DishService : IDishService
 {
     public async Task<IEnumerable<DishDto>> GetDishes(PageParameters pageParameters)
@@ -11,9 +13,10 @@ public class DishService : IDishService
                 .Take(pageParameters.PageSize)
                 .ToListAsync()).Select(Convert)
                 .ToList();
-        }
-    }
 
+        }
+        
+    }
     public async Task<IEnumerable<DishDto>> GetAllDishes()
     {
         using (var db = new DishContext())
@@ -21,8 +24,8 @@ public class DishService : IDishService
             return (await db.Dishes.Include(d => d.Restaurant)
                 .ToListAsync()).Select(Convert);
         }
-    }
 
+    }
     private DishDto Convert(Dish dish)
     {
         var name = dish.Restaurant;
@@ -32,8 +35,7 @@ public class DishService : IDishService
             Price = dish.Price,
             Availability = dish.Availability,
             DishID = dish.DishId,
-            Restaurant = new RestaurantForDishDto
-            {
+            Restaurant = new RestaurantForDishDto {
                 Name = dish.Restaurant.Name
             }
         };
@@ -42,31 +44,5 @@ public class DishService : IDishService
     public DishDto GetDish(OrderDto orderDto, Task<IEnumerable<DishDto>> dishDtos)
     {
         return dishDtos.Result.ToList().FirstOrDefault(d => Equals(d.DishID, orderDto.DishId));
-    }
-
-    public async Task<IEnumerable<DishDto>> FilterDishes(
-        Task<IEnumerable<DishDto>> dishDtos,
-        string dishNameReading,
-        string dsihIdReading,
-        string maxPriceReading,
-        string minPriceReading,
-        string restaurantNameReading,
-        string restaurantIdReading,
-        string availabilityReading)
-    {
-        var filterDishes = dishDtos.Result.ToList().AsEnumerable();
-        if (!String.IsNullOrEmpty(dishNameReading))
-        {
-            filterDishes = filterDishes.Where(fd => fd.Name.Contains(dishNameReading));
-        }
-    }
-
-    private IEnumerable<DishDto> UseSingielFilter(IEnumerable<DishDto> dishes, string namedishAtribute, string reading)
-    {
-        if (!String.IsNullOrEmpty(reading))
-        {
-            return dishes.Where(dishAtribute.Contains(reading));
-        }
-        return dishes;
     }
 }
--- MinimalAPI/Services/RestaurantService.cs
+++ MinimalAPI/Services/RestaurantService.cs
@@ -1,21 +1,23 @@
-﻿using MinimalAPI;
+﻿
+using Microsoft.EntityFrameworkCore;
 
+using MinimalAPI;
 public class RestaurantService : IRestaurantService
 {
     public async Task<IEnumerable<RestaurantDto>> GetRestaurants(PageParameters pageParameters)
     {
         using (var db = new DishContext())
         {
-            var restaurantDtos = (await db.Restaurants
+
+            var enumerable = (await db.Restaurants
                 .Skip((pageParameters.PageNumber - 1) * pageParameters.PageSize)
                 .Take(pageParameters.PageSize)
                 .Include(r => r.Dishes)
                 .ToListAsync())
                 .Select(Convert);
-            return restaurantDtos;
+            return enumerable;
         }
     }
-
     private RestaurantDto Convert(Restaurant restaurant)
     {
         var restaurantDto = new RestaurantDto
@@ -29,13 +31,6 @@ public class RestaurantService : IRestaurantService
 
     private DishForRestaurantDto ConvertDish(Dish dish)
     {
-        return new DishForRestaurantDto { Name = dish.Name };
-    }
-
-    public async Task<IEnumerable<RestaurantDto>> SearchForRestaurants(
-        Task<IEnumerable<RestaurantDto>> restaurantDtos,
-        string searchRestaurant)
-    {
-        return restaurantDtos;
+        return new DishForRestaurantDto { Name = dish.Name} ;
     }
 }
